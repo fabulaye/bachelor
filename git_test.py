be98c3d342475dcb62851c23221da325225d661c
fabulaye <53220111+fabulaye@users.noreply.github.com>
2023-06-05 15:05:21 +0200

    wieder mal gechanged

--- git_test.py
+++ git_test.py
@@ -8,9 +8,32 @@ nba_season_85_86=requests.get(nba_season_85_86_url)
 
 nba_season_85_86=nba_season_85_86.text
 nba_season_85_86=nba_season_85_86.replace(".","")
-print(nba_season_85_86)
+nba_season_85_86=nba_season_85_86.replace("Total","").replace("$","").replace(",","")
+
+nba_season_85_86_split=nba_season_85_86.split("\n")
+print(nba_season_85_86) #tested den bumms
 
 
-#test=re.search(r"\w\w",nba_season_85_86)
+test=re.search("[A-Z]",nba_season_85_86)
 
-#print(test.string)
+print(test.string)
+
+class player():
+    def __init__(self,name,salary_dic) -> None:
+        self.name=name
+        self.salary_dic=salary_dic
+
+
+salary_dict={}#player als key dictionary als value, dic enthält saisons als key und gehalt als value
+
+spieler_liste=[]
+
+def data_cleaning_nba(text):
+    for word in text.split():
+        try: 
+            int(word)
+        except:
+            spieler_liste.append(word)
+
+data_cleaning_nba(nba_season_85_86)
+print(spieler_liste)
