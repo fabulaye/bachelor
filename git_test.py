c642e93930d0f5fec285bcc04518968b286dc743
fabulaye <53220111+fabulaye@users.noreply.github.com>
2023-06-08 16:13:42 +0200

    fixed the whitespaces, dict now working

--- git_test.py
+++ git_test.py
@@ -6,7 +6,7 @@ import os
 
 "Los Angeles Lakers","New Jersey Nets","Philadelphia 76ers","New York Knicks","Boston Celtics","Seattle Supersonics","Los Angeles Clippers","Detroit Pistons","Chicago Bulls","Washington Bullets", "Goldon State Warriors", "Houston Rockets", "Phoenix Suns","San Antonio Spurs", "Dallas Mavericks", "Sacramento Kings","Cleveland Cavaliers", "Golden State Warriors", "Portland Trailblazers","Indiana Pacers", "Atlanta Hawks", "Milwaukee Bucks","Denver Nuggets","Utah Jazz"
 os.chdir("C:/Users/lukas/Desktop/bachelor/bachelor")
-string_pattern=re.compile("[A-Z]{1}[\.a-z']*.[A-Z]{1}[\.a-z']*.[A-Z]*[.a-z']*.[A-Z]*[.a-z']*")
+string_pattern=re.compile("[A-Z]{1}[\.a-z']*.[A-Z,1-9]{1}[\.a-z',1-9]*.[A-Z]*[.a-z']*.[A-Z]*[a-z]*")
 salary_pattern=re.compile("\d+,\d+,*\d*")
 delete_list=['Michael Siewenie for', 'Team Payrolls','Patricia Bender']
 #team_list=team_list.split()
@@ -37,37 +37,62 @@ with open('season85.txt', 'w') as fw:
     
 
 def strip_data(text):
-    text.replace(".","")
-    text.replace("Total","")
-    text.replace("$","")
-    text.replace(",","")
-    #for team in team_list:
-        #text.replace(team,"")
-        #print(team)
+    for string in text:#input is liste
+         for character in string:
+             print(character)
+             if character==".":
+                 del character
+         string.strip()
+         
     return text
 
 with open("season85.txt","r") as f:
     nba_season_85_86=f.readlines()
 
 
+#nba_season_85_86=strip_data(nba_season_85_86)
 nba_season_85_86=str(nba_season_85_86)
 
 
-def clean_data(list):
-    for item in list:
+nba_season_85_86=nba_season_85_86.replace(".","")
+
+
+
+
+
+def clean_data(text):
+    for item in text.split():
         if item in delete_list:
-            list.remove(item)
-    return list        
+            text.remove(item)
+    return text 
+
+
 
 results=string_pattern.findall(nba_season_85_86)
+results.pop()
 
+
+def strip_whitespace(list):
+    new_list=[]
+    for string in list:
+        string=string[:-2]
+        new_list.append(string)
+    return new_list    
+
+results=strip_whitespace(results)
 
 
 class player():
     def __init__(self,name,salary_dic) -> None:
+        id=id
         self.name=name
         self.salary_dic=salary_dic
 
+class season():
+    def __init__(self) -> None:
+        self.id=id
+        self.year=year
+        self.salary_dict=dictionary
 
 salary_dict={}#player als key dictionary als value, dic enthält saisons als key und gehalt als value
 
@@ -85,8 +110,8 @@ def int_data(text):
 
 
 salaries=salary_pattern.findall(nba_season_85_86)
-#print(salaries)
-print(len(salaries))
+
+
 
 
 def extract_names(text):
@@ -109,3 +134,7 @@ def extract_names(text):
 
 nba_season_cleaned=extract_names(nba_season_85_86)
 #print(nba_season_cleaned)
+season_85_dict=dict(zip(results,salaries))
+
+print(season_85_dict["Larry Bird"])
+nba_dict={85:season_85_dict}
