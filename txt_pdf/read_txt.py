476dcc40b3a69f0f6bfc5176c9024330270c68d1
fabulaye <53220111+fabulaye@users.noreply.github.com>
2024-02-24 14:12:53 +0100

    ukraine

--- txt_pdf/read_txt.py
+++ txt_pdf/read_txt.py
@@ -1,5 +1,7 @@
+from file_manager.list_to_string import list_to_string
 
-def read_txt(path):
+def txt_to_str(path):
   with open(path,"r") as f:
       text=f.readlines()
-  return text   
+  text=list_to_string(text)
+  return text
